84da31186a5f17accb723fbd43afd68f9a95bf73
paianjana <53474120+paianjana@users.noreply.github.com>
2020-06-30 11:00:32 +0530

    Add files via upload

---
+++ prgm3_StudentDetails.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+int i;
+int n;
+class student
+{
+    private:
+        string name[10];
+        int id[10];
+        char sec[10];
+    public:
+        void studentDetails()
+        {
+            for(i=0;i<n;i++)
+            {
+                cout<<"Name=";
+                cin>>name[i];
+                cout<<"ID=";
+                cin>>id[i];
+                cout<<"Section=";
+                 cin>>sec[i];
+            }
+        }
+        void display()
+        {
+            cout<<"Name\tID\tSection"<<endl;
+             for(i=0;i<n;i++)
+            {
+                cout<<name[i]<<"\t"<<id[i]<<"\t"<<sec[i]<<endl;
+            }
+        }
+};
